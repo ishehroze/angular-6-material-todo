bfad7a8a315f89d76e288665b7f63a39a29eab4b
Shehroze Islam <shehroze@mislbd.com>
2024-11-07 08:03:44 +0600

    Added todo-container component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoContainerComponent } from './todo-container/todo-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoContainerComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
