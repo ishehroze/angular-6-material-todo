c5d593f23777130cdc93d750dd4c754488006ced
Shehroze Islam <shehroze@mislbd.com>
2024-11-11 04:55:51 +0600

    UI prototype for todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material';
 import { MatCardModule } from '@angular/material';
+import { MatListModule } from '@angular/material';
+import { MatDividerModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,7 +18,9 @@ import { TodoContainerComponent } from './todo-container/todo-container.componen
     BrowserModule,
     BrowserAnimationsModule,
     MatTabsModule,
-    MatCardModule
+    MatCardModule,
+    MatListModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
